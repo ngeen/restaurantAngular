6c77ae38cbea048791a61370cbbc62abc21af19a
engin <o.enginoz@gmail.com>
2018-10-04 23:58:42 +0300

    düzenleme yapıldı

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -4,6 +4,8 @@ import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
 import { LayoutService } from '../../../@core/data/layout.service';
+import {filter, map} from "rxjs/internal/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'ngx-header',
@@ -16,18 +18,40 @@ export class HeaderComponent implements OnInit {
 
   user: any;
 
+  redirectDelay: number = 0;
+
   userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
 
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
               private analyticsService: AnalyticsService,
-              private layoutService: LayoutService) {
+              private layoutService: LayoutService,
+              protected router: Router) {
   }
 
   ngOnInit() {
     this.userService.getUsers()
       .subscribe((users: any) => this.user = users.nick);
+
+    this.menuService.onItemClick()
+      .pipe(
+        filter(({ tag }) => tag === 'userMenu'),
+        map(({ item: { title } }) => title),
+      )
+      .subscribe(title => {
+        if(title === "Log out")
+          this.redirect('/auth/logout')
+        else if(title === "Profile")
+          this.redirect('/profile');
+      });
+
+  }
+
+  redirect(redirect){
+    setTimeout(() => {
+      return this.router.navigateByUrl(redirect);
+    }, this.redirectDelay);
   }
 
   toggleSidebar(): boolean {
--- src/app/menu-list/menu-list.component.ts
+++ src/app/menu-list/menu-list.component.ts
@@ -7,16 +7,28 @@ import { ItemControllerService } from "../@rest";
   styleUrls: ["./menu-list.component.scss"]
 })
 export class MenuListComponent implements OnInit {
+
+  data : any = [];
+  items: any = [];
+  breadCrumb
   constructor(private itemService: ItemControllerService) {}
 
   ngOnInit() {
     this.itemService.getAllItemsUsingGET().subscribe(
       data => {
         console.log(data);
+        this.data = data.payload;
+        this.items = this.data.filter(f => f.itemType == "MENU");
+        console.log(this.items);
       },
       error => {
         console.log(error);
       }
     );
+  }
+
+  onSelect(item: any){
+    if(item.itemType != 'PRODUCT')
+      this.items = this.data.filter(f => f.parentId ==item.id);
   }
 }
